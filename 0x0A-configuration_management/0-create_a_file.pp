bab1435e3a8d15baf6e6b0ed1ee726d9340fe5db
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-12-09 21:13:59 +0000

    [TASK] Config Management initial commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Create a file with puppet
+file {'/tmp/holberton':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
