80422e4ef31ef775bafb9b74655db4ba03934a77
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-02-03 20:01:56 -0500

    [TASK] Grab data and safe to file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+from csv import DictWriter
+
+
+# creates CSV file - takes in dictionary
+def write_to_csv(_data, _id):
+    """takes in data as a dictionary, and the id for name """
+    with open("{}.csv".format(_id), "w", encoding='utf-8') as file:
+        headers = ["USER_ID", "USERNAME",
+                   "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        csv_writer = DictWriter(file, fieldnames=headers)
+        for apt in _data:
+            csv_writer.writerow(apt)
+    return
+
+if __name__ == "__main__":
+    _id = argv[1]
+
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user_url = "{}users/{}".format(base_url, _id)
+    tasks_url = "{}todos?userId={}".format(base_url, _id)
+
+    response = requests.get(user_url)
+    use_info = response.json()
+    user_name = use_info["name"]
+
+    res = requests.get(tasks_url)
+    tasks = res.json()
+
+    _data = []
+    for task in tasks:
+        users_info = {"USER_ID": _id,
+                      "USERNAME": user_name,
+                      "TASK_COMPLETED_STATUS": task["completed"],
+                      "TASK_TITLE": task["title"]}
+        _data.append(users_info)
+
+    write_to_csv(_data, _id)
