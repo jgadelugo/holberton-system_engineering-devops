2be5e51a77ec25026f457a86149ec02197740ff4
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-02-28 00:23:42 -0500

    bugfix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 #fix os error change config
 exec { 'change_config':
-  command => "sudo /bin/sed -i "s/nofile 4/nofile 6000/g; s/nofile 5/nofile 6000/g" /etc/security/limits.conf",
+  command => "sudo /bin/sed -i 's/nofile 4/nofile 6000/g; s/nofile 5/nofile 6000/g' /etc/security/limits.conf",
 }
 
