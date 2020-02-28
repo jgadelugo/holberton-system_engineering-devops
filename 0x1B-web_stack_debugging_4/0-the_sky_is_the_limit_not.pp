3f41e8d516efcf8b81fd89ce6d55a3eef0208b66
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-02-28 00:22:12 -0500

    Add ,

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # puppet code - use strace to find out why apache is returning a 500 error
 
 exec { 'fix_error':
-  command => "/bin/sed -i 's/15/4096/g' /etc/default/nginx; ; sudo service nginx restart" 
+  command => "/bin/sed -i 's/15/4096/g' /etc/default/nginx; ; sudo service nginx restart",
 }
